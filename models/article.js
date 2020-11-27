d3dba2e0feaa8edd0207e113ae03d7520ffc934a
Julia Kurianova <judy@sfdev.com>
2020-11-27 15:53:14 -0500

    Create a schema and model for User and Article

---
+++ models/article.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+// const linkValidator = require('../utils/linkValidator');
+
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+
+  title: {
+    type: String,
+    required: true,
+  },
+
+  text: {
+    type: String,
+    required: true,
+  },
+
+  date: {
+    type: String,
+    required: true,
+  },
+
+  source: {
+    type: String,
+    required: true,
+  },
+
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => linkValidator(link),
+      message: (props) => `${props.value} is not a valid link. `,
+    },
+  },
+
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => linkValidator(link),
+      message: (props) => `${props.value} is not a valid link. `,
+    },
+  },
+
+  owner: {
+    type: 'ObjectId',
+    required: true,
+    // in order to database won't be returned this field by default
+    select: false,
+  },
+
+});
+
+module.exports = mongoose.model('card', articleSchema);
---
+++ models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+// const { AuthError } = require('../errors/auth-err');
+// const linkValidator = require('../utils/linkValidator');
+
+const userSchema = new mongoose.Schema({
+  // must be validate against to email schema
+  email: {
+    required: true,
+    type: String,
+    unique: true,
+  },
+  password: {
+    required: true,
+    type: String,
+    // minlength: 8,
+    // in order to user's password hash won't be returned from the database by default
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password, next) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new AuthError('Incorrect email or password');
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new AuthError('Incorrect email or password');
+          }
+          return user;
+        });
+    })
+    .catch((err) => next(err));
+};
+
+module.exports = mongoose.model('user', userSchema);
