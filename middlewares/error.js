b1ee60fc0f321d2167c727002d6f6f1fa87e59f2
Julia Kurianova <judy@sfdev.com>
2020-12-07 19:05:42 -0500

    Fix status code bug

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,8 +1,8 @@
 const { httpStatusCode } = require('../utils/consts');
 
 module.exports = ((err, req, res, next) => {
-  const { statusCode = httpStatusCode.SERVER_ERROR, message = 'An error occurred on the server' } = err;
   // if an error has no status, display 500
-  res.status(httpStatusCode.SERVER_ERROR).send({ statusCode, message });
+  const { statusCode = httpStatusCode.SERVER_ERROR, message = 'An error occurred on the server' } = err;
+  res.status(err.statusCode).send({ statusCode, message });
   next();
 });
